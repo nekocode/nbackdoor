3aff31836c85ccd637f041e0c8b7f278fb79ec50
nekocode <syfyw@qq.com>
2015-09-17 22:49:18 +0800

    wrap hide cmd to a function

--- client.py
+++ client.py
@@ -2,10 +2,12 @@ import time
 import ctypes
 __author__ = 'nekocode'
 
-whnd = ctypes.windll.kernel32.GetConsoleWindow()
-if whnd != 0:
-    ctypes.windll.user32.ShowWindow(whnd, 0)
-    ctypes.windll.kernel32.CloseHandle(whnd)
+def hide_cmd():
+    whnd = ctypes.windll.kernel32.GetConsoleWindow()
+    if whnd != 0:
+        ctypes.windll.user32.ShowWindow(whnd, 0)
+        ctypes.windll.kernel32.CloseHandle(whnd)
 
-while True:
-    time.sleep(10)
+if __name__ == '__main__':
+    while True:
+        time.sleep(10)
