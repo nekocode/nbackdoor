84172566b5614f430c0125c77fdccce933603850
nekocode <syfyw@qq.com>
2015-10-13 14:55:49 +0800

    update console_output_redirect

--- console_output_redirect.py
+++ console_output_redirect.py
@@ -1,23 +1,18 @@
 import sys
 import win32api
 from subprocess import Popen, PIPE
+
 __author__ = 'nekocode'
 
 
-def ctrl_handler(ctrl_type):
-    return True
-
-
 def main():
-    win32api.SetConsoleCtrlHandler(ctrl_handler, True)
     while True:
         cmd_input = raw_input('cmd> ')
-        proc = Popen(cmd_input, shell=True, stdout=PIPE, stderr=PIPE, stdin=PIPE)
-        # stdout_value = proc.stdout.read()
-        # stdout_value += proc.stderr.read()
-        sys.stdout = proc.stdout
+        if cmd_input == 'exit':
+            break;
 
-        # p.stdin.write(cmd_input)
+        proc = Popen(cmd_input, shell=True, stdout=sys.stdout, stderr=PIPE, stdin=sys.stdin)
+        proc.wait()
 
 
 if __name__ == '__main__':
